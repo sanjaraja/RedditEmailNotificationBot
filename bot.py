083f41bb802b1b7433918e32ce52d601bac082ca
Sanjay Rajasekaran <rajasekaran.sanjay@gmail.com>
2019-08-25 14:42:40 -0700

    Asking user for information regarding email id for notification and what key words to look for within which subreddits

--- bot.py
+++ bot.py
@@ -1,8 +1,19 @@
-import praw
-from prawCredentials import client_id, client_secret, password, user_agent, username
-from requests import session
+import praw #Importing the praw wrapper
+from prawCredentials import client_id, client_secret, password, user_agent, username #Importing bot credentials from prawCredentials.py
+from requests import session #Need to import this for bot credentials
 
-email_id = input("What is the email id that you want notifications sent to?")
-password = input("What is the password of the email_id that you provided")
-print("Email id", email_id)
-print("Password", password)
+""" Asking for input from user as to which email id the bot needs to notify and what key words to look for in which subreddits"""
+email_id = input("What is the email id that you want notifications sent to?") #Asking for email id
+password = input("What is the password of the email_id that you provided") #Asking for password for email id
+key_words = input("Enter the key words that you want to search for as comma seperated values.").split(",") #Generating array of key words
+subreddits = input("Enter the subreddits that you want to search these key_words for").split(",") #Generating array of subreddits
+
+""" Instantiating the bot which will search for key words within the subreddits mentioned """
+reddit = praw.Reddit(client_id= client_id,
+                     client_secret= client_secret,
+                     password= password,
+                     requestor_kwargs={'session': session},  # pass Session
+                     user_agent= user_agent,
+                     username= username)
+
+
