c0b4e43d3f80b227e7864749aaff963de77edd45
Sanjay Rajasekaran <rajasekaran.sanjay@gmail.com>
2019-08-25 14:30:29 -0700

    Added basic imports for main class and added gitignore file

---
+++ bot.py
@@ -0,0 +1,3 @@
+import praw
+from prawCredentials import client_id, client_secret, password, user_agent, username
+print(client_id)
---
+++ prawCredentials.py
@@ -0,0 +1,5 @@
+client_id = '_75AOylrmYBKYQ'
+client_secret = '80_eYmAMn28_pLdTHBMzESWJjxU'
+password = 'python*101'
+user_agent = "reddit_notification_bot"
+username = 'redditEmailNotifier'
