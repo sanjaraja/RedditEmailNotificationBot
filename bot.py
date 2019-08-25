bad667c18f0ce9c88b24333ac50721dffca960d8
Sanjay Rajasekaran <rajasekaran.sanjay@gmail.com>
2019-08-25 15:15:29 -0700

    Put Code into main method

--- bot.py
+++ bot.py
@@ -1,14 +1,16 @@
 import praw #Importing the praw wrapper
 from prawCredentials import client_id, client_secret, password, user_agent, username #Importing bot credentials from prawCredentials.py
-from requests import session #Need to import this for bot credentials
+from requests import Session #Need to import this for bot credentials
 
-""" Asking for input from user as to which email id the bot needs to notify and what key words to look for in which subreddits"""
+def main():
+    """ Asking for input from user as to which email id the bot needs to notify and what key words to look for in which subreddits"""
 email_id = input("What is the email id that you want notifications sent to?") #Asking for email id
 password = input("What is the password of the email_id that you provided") #Asking for password for email id
 key_words = input("Enter the key words that you want to search for as comma seperated values.").split(",") #Generating array of key words
 subreddits = input("Enter the subreddits that you want to search these key_words for").split(",") #Generating array of subreddits
 
 """ Instantiating the bot which will search for key words within the subreddits mentioned """
+session = Session()
 reddit = praw.Reddit(client_id= client_id,
                      client_secret= client_secret,
                      password= password,
@@ -16,4 +18,6 @@ reddit = praw.Reddit(client_id= client_id,
                      user_agent= user_agent,
                      username= username)
 
+if __name__ == "__main__":
+    main()
 
