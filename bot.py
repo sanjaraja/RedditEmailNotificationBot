5bc7460792ae9a84110c2d294f67e93116028748
Sanjay Rajasekaran <rajasekaran.sanjay@gmail.com>
2019-08-25 14:33:44 -0700

    Added feature where user can input email id and password

--- bot.py
+++ bot.py
@@ -1,3 +1,8 @@
 import praw
 from prawCredentials import client_id, client_secret, password, user_agent, username
-print(client_id)
+from requests import session
+
+email_id = input("What is the email id that you want notifications sent to?")
+password = input("What is the password of the email_id that you provided")
+print("Email id", email_id)
+print("Password", password)
