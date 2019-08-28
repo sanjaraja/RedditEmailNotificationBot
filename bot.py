4c99aa291a06a6b05ccc45c640066302a9f770c5
Sanjay Rajasekaran <rajasekaran.sanjay@gmail.com>
2019-08-27 18:08:17 -0700

    Fixed issues regarding the processing of subredditString and added streamProcess method which returns a dictionary of info that needs to be sent as an email

--- bot.py
+++ bot.py
@@ -2,10 +2,21 @@ import praw #Importing the praw wrapper
 from prawCredentials import client_id, client_secret, password, user_agent, username #Importing bot credentials from prawCredentials.py
 from requests import Session #Need to import this for bot credentials
 
+def streamProcess(bot, key_words): #Need to pass in the instantiated bot and the key words that need to be searched
+    return_dic = {}
+    for submission in bot.stream.submissions():
+        for word in key_words:
+            if word.lower() in submission.title.lower():
+                item = [submission.title, submission.score, submission.permalink]
+                return_dic[submission.title] = item
+
+        if len(return_dic) == 10: return return_dic
+
+
 def main():
     """ Asking for input from user as to which email id the bot needs to notify and what key words to look for in which subreddits"""
-email_id = input("What is the email id that you want notifications sent to?") #Asking for email id
-password = input("What is the password of the email_id that you provided") #Asking for password for email id
+#email_id = input("What is the email id that you want notifications sent to?") #Asking for email id
+#password = input("What is the password of the email_id that you provided") #Asking for password for email id
 key_words = input("Enter the key words that you want to search for as comma seperated values.").split(",") #Generating array of key words
 subreddits = input("Enter the subreddits that you want to search these key_words for").split(",") #Generating array of subreddits
 
@@ -18,13 +29,13 @@ reddit = praw.Reddit(client_id= client_id,
                      user_agent= user_agent,
                      username= username)
 
-subredditString = "+".join(subreddits).replace(" ", "")
+subredditString = "+".join(subreddits).replace(" ", "").strip()#Joining subreddits that need to be searched with a + 
+#subredditString = subredditString.strip()
 print(subredditString)
+bot = reddit.subreddit(subredditString) #Creating a subreddit instance with the subreddits that need to be traversed
+dic = streamProcess(bot, key_words)
+print(dic)
 
-
-
-def streamProcess(bot):
-    print("hello")
 
 if __name__ == "__main__":
     main()
